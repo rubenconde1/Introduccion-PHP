04bd30434b3c224988d74c653d60b34f777a0431
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-09-28 09:10:43 +0200

    cookie

---
+++ cookie.php
@@ -0,0 +1,28 @@
+<?php
+
+    $language = $_GET["setLanguage"] ?? $_COOKIE["language"] ?? "es";
+    setcookie("language", $language, time() + 30 * 24 * 60 * 60);
+
+	if ($language == "en"){
+		$content = "This page is in English";
+        $title = "Change the language of the page";
+    }else{
+		$content = "Esta página está en Castellano (Idioma por defecto)";
+        $title = "Cambiar el idioma de la página";
+    }
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+    <meta name="author" content="Víctor Ponz">
+</head>    
+<body>
+    <ul><?= $title ?>
+        <li><a href='cookie.php?setLanguage=es'>Español</a></li>
+        <li><a href='cookie.php?setLanguage=en'>Inglés</a></li>
+    </ul>
+    <?= $content ?>
+</body>
+</html>

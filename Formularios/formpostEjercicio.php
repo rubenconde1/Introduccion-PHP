bbb9fbe9fa4196fe33685d645646ec540cea5065
AdrianBellmunt <adrianbellmuntorero@gmail.com>
2022-10-03 13:52:33 +0200

    03-10-22

---
+++ Formularios/formpostEjercicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario</title>
+</head>
+<body>
+    Hola <?php echo $_GET["nombre"]; ?><br>
+    Tu email es: <?php echo $_GET["email"]; ?><br>
+    Educacion:<?php echo $_GET["educacion"]?><br>
+    Avatar <?php echo $_GET["imagen"]?><br>
+    
+</body>
+</html>
